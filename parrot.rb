ef3dead72a5546ba822424f1d9876b045fd3e719
Will Corcoran <corcoranwill@gmail.com>
2020-02-13 01:14:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase)
-  if arg == nil
+  if phrase == nil
     phrase = "Squawk!"
   else
     phrase = "Pretty bird!"
